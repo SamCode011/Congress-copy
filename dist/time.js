5f0829e6301b86318253c70506f689569b9d5a06
SamCode011 <aaron2000-2000@outlook.com>
2023-11-14 16:33:21 -0500

    timeexcep

---
+++ dist/time.js
@@ -0,0 +1,33 @@
+const getRemainTime = deadline => {
+    let now = new Date(),
+        remainTime    = (new Date(deadline) - now + 1000) / 1000,
+        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+        remainHours   = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
+        remainDays    = Math.floor(remainTime / (3600 *24));
+
+        return {
+            remainTime ,
+            remainSeconds,
+            remainMinutes ,
+            remainHours ,
+            remainDays
+
+
+        }
+    }
+
+const countdown = (deadline,elem,finalMessage) => {
+    const el = document.getElementById(elem);
+    const timerUpdate = setInterval ( () => {
+        let t = getRemainTime(deadline);
+        el.innerHTML = `${t.remainDays}d:${t.remainHours}h:${t.remainMinutes}m:${t.remainSeconds}s`;
+
+        if (t.remainTime <= 1) {
+            clearInterval (timerUpdate);
+            el.innerHTML = finalMessage;
+        }
+    },1000)
+}
+countdown(' Nov 14 2024 22:50:06 GMT-0500','count-reverse','final')
+/* console.log(getRemainTime('Nov 08 2023 22:25:06 GMT-0500')) */
