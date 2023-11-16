f252488aa5a2b0564a361b176139ccfe003d181a
SamCode011 <aaron2000-2000@outlook.com>
2023-11-16 15:50:22 -0500

    update nav

---
+++ dist/navs.js
@@ -0,0 +1,16 @@
+let listElements = document.querySelectorAll('.list_button--click');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click',()=>{
+
+        listElement.classList.toggle('arrow');
+
+        let height = 0 ;
+        let menu =listElement.nextElementSibling;
+       /*  console.log(menu.scrollHeight); */
+       if(menu.clientHeight=="0") {
+        height=menu.scrollHeight;
+       }
+       menu.style.height= `${height}px` 
+    })
+})
