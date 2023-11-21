698c25490df055db7c4de27d2e27fc0bf0feeef0
SamCode011 <aaron2000-2000@outlook.com>
2023-11-20 19:40:23 -0500

    update nav.js

--- dist/navs.js
+++ dist/navs.js
@@ -2,22 +2,29 @@ let listElements = document.querySelectorAll('.list_button--click');
 
 listElements.forEach(listElement => {
     listElement.addEventListener('click',()=>{
+        activation.classList.remove('active');
+        let principal = activation.nextElementSibling;
+        principal.style.height = '0';
 
         listElement.classList.toggle('arrow');
 
         let height = 0 ;
-        let menu =listElement.nextElementSibling;
+        let menu =listElement.nextElementSibling; 
        /*  console.log(menu.scrollHeight); */
        if(menu.clientHeight=="0") {
         height=menu.scrollHeight;
        }
        menu.style.height= `${height}px` 
     })
-})
+}) 
  
  let navActive = document.querySelectorAll('.click-active');
 
 navActive.forEach(activation => {
+    activation.classList.remove('active');
+      let principal = activation.nextElementSibling;
+      principal.style.height = '0';
+
     activation.addEventListener('click',()=> {
         activation.classList.toggle('active');
         let height = 0 ;
@@ -28,5 +35,45 @@ navActive.forEach(activation => {
         principal.style.height=height;
     })
 })
+/* document.addEventListener('DOMContentLoaded', function () {
+    let navActive = document.querySelectorAll('.click-active');
+
+    navActive.forEach(activation => {
+      activation.classList.remove('active');
+      let principal = activation.nextElementSibling;
+      principal.style.height = '0';
+
+      activation.addEventListener('click', () => {
+        activation.classList.toggle('active');
+        if (activation.classList.contains('active')) {
+          principal.style.height = principal.scrollHeight + 'px';
+        } else {
+          principal.style.height = height;
+        }
+      })
+    });
+  });
+ */
+
+/*   document.addEventListener('DOMcontentLoaded',function() {
+    let navActive = document.querySelectorAll('.click-active');
+
+    navActive.forEach(activation => {
+        activation.classList.remove('active');
+        let principal = activation.nextElementSibling;
+        principal.style.height='0';
+
+        activation.addEventListener('click',()=> {
+            activation.classList.toggle('active');
+            if (activation.classList.contains('active')) {
+                principal.style.height=principal.scrollHeight +'px';
+            }else {
+                principal.style.height=height;
+            }
+            
+        })
+    })
+})
+  */
    
  
