9f9b6fc600da15d86f52e1c500e93aa317924226
SamCode011 <aaron2000-2000@outlook.com>
2023-11-25 13:49:28 -0500

    update html ,update nav.js

--- dist/navs.js
+++ dist/navs.js
@@ -1,18 +1,4 @@
-/* Submenu */
-let listElements = document.querySelectorAll('.list_button--click');
-
-listElements.forEach(listElement => {
-    listElement.addEventListener('click',()=>{
-        listElement.classList.toggle('arrow');
-        let height = 0 ;
-        let menu =listElement.nextElementSibling; 
-       if(menu.clientHeight=="0") {
-        height=menu.scrollHeight;
-       }
-       menu.style.height= `${height}px` 
-    })
-}) 
- /* menu */
+/* menu */
  let navActive = document.querySelectorAll('.click-active');
 
 navActive.forEach(activation => {
@@ -30,6 +16,20 @@ navActive.forEach(activation => {
         principal.style.width=width;
     })
 })
+/* Submenu */
+let listElements = document.querySelectorAll('.list_button--click');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click',()=>{
+        listElement.classList.toggle('arrow');
+        let height = 0 ;
+        let menu =listElement.nextElementSibling; 
+       if(menu.clientHeight=="0") {
+        height=menu.scrollHeight;
+       }
+       menu.style.height= `${height}px` 
+    })
+}) 
 /* submenu-image */
 document.addEventListener('DOMContentLoaded',function() {
     let img = document.getElementById('bars');
