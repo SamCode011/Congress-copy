7fc7c4ff71b6890c9bc36c213ff2a4c63bed82fd
SamCode011 <aaron2000-2000@outlook.com>
2023-11-25 12:56:54 -0500

    update nav.html,update nav.js

--- dist/navs.js
+++ dist/navs.js
@@ -1,79 +1,51 @@
+/* Submenu */
 let listElements = document.querySelectorAll('.list_button--click');
 
 listElements.forEach(listElement => {
     listElement.addEventListener('click',()=>{
-      /*   activation.classList.remove('active');
-        let principal = activation.nextElementSibling;
-        principal.style.height = '0'; */
-
         listElement.classList.toggle('arrow');
-
         let height = 0 ;
         let menu =listElement.nextElementSibling; 
-       /*  console.log(menu.scrollHeight); */
        if(menu.clientHeight=="0") {
         height=menu.scrollHeight;
        }
        menu.style.height= `${height}px` 
     })
 }) 
- 
+ /* menu */
  let navActive = document.querySelectorAll('.click-active');
 
 navActive.forEach(activation => {
     activation.classList.remove('active');
       let principal = activation.nextElementSibling;
-      principal.style.height = '0';
+      principal.style.width = '0';
 
     activation.addEventListener('click',()=> {
         activation.classList.toggle('active');
-        let height = 0 ;
+        let width = 0 ;
         let principal = activation.nextElementSibling;
-        if (principal.clientHeight=="0") {
-            height=null ;
+        if (principal.clientWidth=="0") {
+            width=null ;
         }
-        principal.style.height=height;
+        principal.style.width=width;
     })
 })
-/* document.addEventListener('DOMContentLoaded', function () {
-    let navActive = document.querySelectorAll('.click-active');
+/* submenu-image */
+document.addEventListener('DOMContentLoaded',function() {
+    let img = document.getElementById('bars');
+    img.addEventListener('click',function() {
+      img.classList.toggle('oculto');
+       
 
-    navActive.forEach(activation => {
-      activation.classList.remove('active');
-      let principal = activation.nextElementSibling;
-      principal.style.height = '0';
+       
+       })
+    })
 
-      activation.addEventListener('click', () => {
-        activation.classList.toggle('active');
-        if (activation.classList.contains('active')) {
-          principal.style.height = principal.scrollHeight + 'px';
-        } else {
-          principal.style.height = height;
-        }
-      })
-    });
-  });
- */
-
-/*   document.addEventListener('DOMcontentLoaded',function() {
-    let navActive = document.querySelectorAll('.click-active');
-
-    navActive.forEach(activation => {
-        activation.classList.remove('active');
-        let principal = activation.nextElementSibling;
-        principal.style.height='0';
-
-        activation.addEventListener('click',()=> {
-            activation.classList.toggle('active');
-            if (activation.classList.contains('active')) {
-                principal.style.height=principal.scrollHeight +'px';
-            }else {
-                principal.style.height=height;
-            }
-            
+    document.addEventListener('DOMContentLoaded',function (){
+        let ghostElement =document.querySelector('.ghost');
+        let triggerElement =document.querySelector('.ghost-click');
+        triggerElement.addEventListener('click',function (){
+            ghostElement.style.display =ghostElement.style.display ==='none' ? 'flex' : 'none';
         })
     })
-})
-  */
-   
  
